2456a898050a3e7d2d2380659bff4163954902c7
bastikk <b15042002@gmail.com>
2023-01-14 06:34:21 +0200

    Lab3, Lab4, Lab5

---
+++ Lab3-6/Assets/Scripts/GameManager.cs
@@ -0,0 +1,109 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public Ball ball { get; private set; }
+    public Paddle paddle { get; private set; }
+    public Brick[] bricks { get; private set; }
+
+    const int NUM_LEVELS = 2;
+
+    public int level = 1;
+    public int score = 0;
+    public int lives = 3;
+
+    private void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+
+        SceneManager.sceneLoaded += OnLevelLoaded;
+    }
+
+    public void Play()
+    {
+        NewGame();
+    }
+
+    public void Exit()
+    {
+        Exit();
+    }
+
+    private void NewGame()
+    {
+        score = 0;
+        lives = 3;
+
+        LoadLevel(1);
+    }
+
+    private void LoadLevel(int level)
+    {
+        this.level = level;
+
+        if (level > NUM_LEVELS)
+        {
+            LoadLevel(1);
+            return;
+        }
+
+        SceneManager.LoadScene("Level" + level);
+    }
+
+    private void OnLevelLoaded(Scene scene, LoadSceneMode mode)
+    {
+        ball = FindObjectOfType<Ball>();
+        paddle = FindObjectOfType<Paddle>();
+        bricks = FindObjectsOfType<Brick>();
+    }
+
+    public void Miss()
+    {
+        lives--;
+
+        if (lives > 0)
+        {
+            ResetLevel();
+        }
+        else
+        {
+            GameOver();
+        }
+    }
+
+    private void ResetLevel()
+    {
+        paddle.ResetPaddle();
+        ball.ResetBall();
+    }
+
+    private void GameOver()
+    {
+        NewGame();
+    }
+
+    public void Hit(Brick brick)
+    {
+        score += brick.points;
+
+        if (Cleared())
+        {
+            LoadLevel(level + 1);
+        }
+    }
+
+    private bool Cleared()
+    {
+        for (int i = 0; i < bricks.Length; i++)
+        {
+            if (bricks[i].gameObject.activeInHierarchy && !bricks[i].unbreakable)
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+}
